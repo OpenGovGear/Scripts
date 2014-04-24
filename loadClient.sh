3bc27aed397acd914211be73d0e44d72e3912888
Mike Sinclair <mm.sinclair08@gmail.com>
2014-04-23 18:24:45 -0700

    added in git API
    added in some git API

--- loadClient.sh
+++ loadClient.sh
@@ -4,29 +4,42 @@
 #ready to go into prod. It creates a production.ini file, a 
 #database dump file, and loads them as well as any custom plug-ins
 #to git, where they'll be launched into production. The image it runs 
-#on will have GIT installed, the OGG admins ID and password already 
+#on will have GIT installed, the OGG admins ID and password already  <--- not done
 #validated using the credential-helper utility 
 
 
 echo 'Enter the client organization name (lowercase):'
-read orgName
+read orgname
 
-echo "Has a git repository been set up for ${orgName} on OGG(y/n)?"
+echo "Has a git repository been set up for $orgName on OGG(y/n)?"
 read gitconfirm
 
-if [ ${gitconfirm} = "n" ] #give user an out if they forgot
+if [ ${gitconfirm} = "n" ] #create one
 then
-	echo Please create client git repository on OGG then load client
-	exit 1
+	echo Please enter your git username
+	read gitusername
+	
+	echo Please enter the new repo name
+	read reponame
+	
+	echo enter project description
+	read projectdesc
+	    # POST data via git API
+        curl -u "${gitusername}" https://api.github.com/user/repos -d '{"name":"${reponame}","description":"${projectdesc}"}'
+        # add def for location and existance of connect remote repo on github
 fi
 
 #create a git repository connected to OpenGovGear somehow
-sudo mkdir -p /home/${orgName}
-sudo chown `whoami` /home/${orgName}/
-cd /home/${orgName}
+sudo mkdir -p /home/${orgname}
+sudo chown `whoami` /home/${orgname}/
+cd /home/${orgname}
 git init
+git remote add origin https://github.com/m-sinclair/"${reponame}".git
 touch README
-cat "Staging resources for ${orgName}" > README
+echo "Staging resources for ${orgname}" > README
+git add README
+git commit -m 'the script works up to this point'
+git push -u origin master
 
 #Dump database and move dump file to local git repository
 . /usr/lib/ckan/default/bin/activate
