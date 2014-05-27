462def56c85cc6a4afa59faaa051217859f420aa
jared-brown <jared@opengovgear.com>
2014-05-26 20:11:13 -0700

    Update devClient.sh
    use installTheme to rename extension after organization

--- devClient.sh
+++ devClient.sh
@@ -38,7 +38,8 @@
 #####################################################################################
 
 function installTheme {
-	cp -r /home/`whoami`/${orgName}-staging/ckan-plugins/ckanext-${themeName} /home/`whoami`/${orgName}-staging/${orgName}
+	mv -r /home/`whoami`/${orgName}-staging/ckan-plugins/ckanext-${themeName}/ckanext/${themeName} /home/`whoami`/${orgName}-staging/ckan-plugins/ckanext-${themeName}/ckanext/${orgName}_theme
+	mv -r /home/`whoami`/${orgName}-staging/ckan-plugins/ckanext-${themeName} /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${orgName}_theme
 	#cd /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${themeName}
 	#python setup.py develop
 	sudo sed -i "/plugins/ a\ ${themeName} " /etc/ckan/${orgName}/development.ini
