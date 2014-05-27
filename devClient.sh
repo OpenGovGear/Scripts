94af8a85605b443ddd8452f737e359455f1288e9
jared-brown <jared@opengovgear.com>
2014-05-26 20:30:13 -0700

    Update devClient.sh
    change the theme template to a custom theme named for the organization. not sure if there's a better way.

--- devClient.sh
+++ devClient.sh
@@ -38,8 +38,11 @@
 #####################################################################################
 
 function installTheme {
-	mv -r /home/`whoami`/${orgName}-staging/ckan-plugins/ckanext-${themeName}/ckanext/${themeName} /home/`whoami`/${orgName}-staging/ckan-plugins/ckanext-${themeName}/ckanext/${orgName}_theme
-	mv -r /home/`whoami`/${orgName}-staging/ckan-plugins/ckanext-${themeName} /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${orgName}_theme
+	cp -r /home/`whoami`/${orgName}-staging/ckan-plugins/ckanext-${themeName} /home/`whoami`/${orgName}-staging/${orgName}
+	mv /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${themeName}/ckanext/${themeName} /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${themeName}/ckanext/${orgName}_theme
+	mv /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${themeName}/ckanext_${themeName}.egg-info /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${themeName}/ckanext_${orgName}_theme.egg-info
+	mv /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${themeName} /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${orgName}_theme
+	#mv -r /home/`whoami`/${orgName}-staging/ckan-plugins/ckanext-${themeName} /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${orgName}_theme
 	#cd /home/`whoami`/${orgName}-staging/${orgName}/ckanext-${themeName}
 	#python setup.py develop
 	sudo sed -i "/plugins/ a\ ${themeName} " /etc/ckan/${orgName}/development.ini
