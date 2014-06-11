dd70e2fcd0a876ddd1dba9441221805ea0ca5d1a
jared-brown <jared@opengovgear.com>
2014-06-10 23:07:43 -0700

    launch client from git

--- launchClient.sh
+++ launchClient.sh
@@ -3,27 +3,50 @@
 echo "Enter client organization's name(lowercase):"
 read orgName
 
-#echo "Enter the road the client organization's main office is on:"
-#read orgPswd
 
-echo "Enter the host ip address:"
+echo "Have you committed all changes to git and created a production.ini file for this client?(y/n)"
+read confirmgit
+if [ ${confirmgit} = "n" ]
+then
+	echo 'Please committed all changes to git and create a production.ini file before continuing.'
+	exit 1
+fi
+
+echo "Have you created the database user/schema and solr core for this client on the database server?(y/n)"
+read confirmdb
+if [ ${confirmdb} = "n" ]
+then
+	echo 'Please create the database user, schema and password on the database server before continuing.'
+	exit 1
+fi
+
+echo "Enter the local subnet fixed ip for the database server: (n if unknown)"
+read dbserv
+if [ ${dbserv} = "n" ]
+then
+	echo "Please confirm the database server's fixed ip before continuing."
+	exit 1
+fi
+
+echo "Enter the ckan server's public ip address:"
 read hostIP
 
-echo "Enter the database ip address:"
-read dbserv
+#bring in the configuration file and extensions
+git clone https://github.com/OpenGovGear/${orgName}-staging
 
-#sudo cp -r /usr/lib/ckan/default /usr/lib/ckan/${orgName}
-sudo cp /etc/ckan/${orgName}/development.ini /etc/ckan/${orgName}/production.ini
-#cd /etc/ckan/${orgName}
-#sed -i s/default/${orgName}/ production.ini
-#sudo mkdir /etc/ckan/${orgName}
-#bring in the production.ini file
+#point the config file to the database server and make other customizations
+#specific to production server
+sudo cp ./${orgName}-staging/production.ini /etc/ckan/${orgName}/production.ini
 
-#bring in the extensions
+#install this client's extensions into the virtual environment
+sudo cp -r ./${orgName}-staging/ckanext-${orgName}_theme /usr/lib/ckan/default/src
+. /usr/lib/ckan/default/bin/activate
+cd /usr/lib/ckan/default/src/ckanext-${orgName}_theme
+python setup.py develop
 
+#create this client's virtual host files
 sudo cp /etc/ckan/default/apache.wsgi /etc/ckan/${orgName}/apache.wsgi
-cd /etc/ckan/${orgName}
-sudo sed -i s/default/${orgName}/ apache.wsgi
+sudo sed -i "s/default/${orgName}/" /etc/ckan/${orgName}/apache.wsgi
 
 sudo cp /etc/apache2/sites-available/ckan_default /etc/apache2/sites-available/${orgName}
 cd /etc/apache2/sites-available
@@ -33,17 +56,7 @@ sudo sed -i s/default.ckanhosted.com/data.${orgName}.com/ ${orgName}
 sudo sed -i s/default/${orgName}/ ${orgName}
 
 #enable this client's solr core
-sudo sed -i "/<\/cores>/ i\
- <core name=\"${orgName}\" instanceDir=\"${orgName}\"><property name=\"dataDir\" value=\"/var/lib/solr/data/${orgName}\" \/><\/core>" /usr/share/solr/solr.xml
-sudo -u jetty mkdir /var/lib/solr/data/${orgName}
-sudo mkdir /etc/solr/${orgName}
-sudo cp -R /etc/solr/ckan_default/conf /etc/solr/${orgName}/
-sudo rm /etc/solr/${orgName}/conf/schema.xml
-sudo ln -s /usr/lib/ckan/${orgName}/src/ckan/ckan/config/solr/schema.xml /etc/solr/${orgName}/conf/schema.xml
-sudo mkdir /usr/share/solr/${orgName}
-sudo ln -s /etc/solr/${orgName}/conf /usr/share/solr/${orgName}/conf
-sudo service jetty restart
-sudo sed -i "s_8983/solr_8983/solr/${orgName}_" /etc/ckan/${orgName}/development.ini
+sudo sed -i "s_127.0.0.1:8983/solr_${dbserv}:8983/solr/${orgName}_" /etc/ckan/${orgName}/production.ini
 
 sudo a2ensite ${orgName}
 sudo service apache2 restart
